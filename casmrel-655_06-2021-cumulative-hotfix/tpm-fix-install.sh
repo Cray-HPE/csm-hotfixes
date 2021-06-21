aa2c0c4f92c5f2e67579b7cb430bcd9181330e66
Russell Bunch <rbunch@cray.com>
2021-06-21 15:04:53 -0500

    Move into CASMREL folder

--- casmrel-655_06-2021-cumulative-hotfix/tpm-fix-install.sh
+++ casmrel-655_06-2021-cumulative-hotfix/tpm-fix-install.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 echo 'loading repair script onto each NCN..'
 for ncn in $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u | tr -t '\n' ' '); do
-    scp $(dirname $0)/repair.sh $ncn:/tmp/cast-26421.sh
+    scp $(dirname $0)/tpm-fix-repair.sh $ncn:/tmp/cast-26421.sh
 done
 echo 'running repair script..'
 pdsh -b -w $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u | tr -t '\n' ',') '/tmp/cast-26421.sh'
