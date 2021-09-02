d9dd8d5223dffe9bab592bd5717a9dcd19bca4ac
Brad Klein <bklein@cray.com>
2021-09-02 11:28:16 -0600

    More changes to deploy node_exporter

--- CASMREL-755/upgrade.sh
+++ CASMREL-755/upgrade.sh
@@ -13,16 +13,17 @@ source "${ROOTDIR}/lib/version.sh"
 workdir="$(mktemp -d)"
 trap "rm -fr '${workdir}'" EXIT
 
-
-# Patch core-services manifest
-kubectl -n loftsman get cm loftsman-core-services -o jsonpath='{.data.manifest\.yaml}' > "${workdir}/core-services.yaml"
+# Patch platform manifest
+kubectl -n loftsman get cm loftsman-platform -o jsonpath='{.data.manifest\.yaml}' > "${workdir}/platform.yaml"
+# Update cray-sysmgmt-health
+yq w -i "${workdir}/platform.yaml" 'spec.charts.(name==cray-sysmgmt-health).version' 0.12.6
 
 # Patch sysmgmt manifest
 kubectl -n loftsman get cm loftsman-sysmgmt -o jsonpath='{.data.manifest\.yaml}' > "${workdir}/sysmgmt.yaml"
 # Update cray-hms-hmnfd
 yq w -i "${workdir}/sysmgmt.yaml" 'spec.charts.(name==cray-hms-hmnfd).version' 1.7.5
 
-# Update the product catalog to report CSM 0.9.4
+# Update the product catalog to report CSM 0.9.5
 yq w -i "${workdir}/sysmgmt.yaml" 'spec.charts.(name==csm-config).values.cray-import-config.import_job.CF_IMPORT_PRODUCT_VERSION' "$RELEASE_VERSION"
 yq w -i "${workdir}/sysmgmt.yaml" 'spec.charts.(name==csm-config).values.cray-import-config.catalog.image.tag' 0.0.9
 yq w -i "${workdir}/sysmgmt.yaml" 'spec.charts.(name==cray-csm-barebones-recipe-install).values.cray-import-kiwi-recipe-image.import_job.PRODUCT_VERSION' "$RELEASE_VERSION"
@@ -35,8 +36,20 @@ export PDSH_SSH_ARGS_APPEND="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/
 export IFS=","
 for master in $masters; do
   scp ${ROOTDIR}/patch-manifests.sh $master:/tmp
+  pdsh -w $master "/tmp/patch-manifests.sh"
+  # Give K8S a chance to spin up pods for this node
+  sleep 10
 done
-pdsh -w "$masters" "/tmp/patch-manifests.sh"
+unset IFS
+
+# Distribute and configure node-exporter to storage nodes
+num_storage_nodes=$(craysys metadata get num-storage-nodes)
+for node_num in $(seq $num_storage_nodes); do
+  storage_node=$(printf "ncn-s%03d" "$node_num")
+  scp ${ROOTDIR}/files/node_exporter $storage_node:/usr/bin
+  scp ${ROOTDIR}/install-node_exporter-storage.sh $storage_node:/tmp
+  pdsh -w $storage_node "/tmp/install-node_exporter-storage.sh"
+done
 
 function deploy() {
     while [[ $# -gt 0 ]]; do
@@ -44,6 +57,9 @@ function deploy() {
         shift
     done
 }
+
+# Redeploy platform
+deploy "${workdir}/platform.yaml"
 
 # Redeploy sysmgmt
 deploy "${workdir}/sysmgmt.yaml"
