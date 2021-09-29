de92cdf1bf16aa90bd45217de9e58f5d38c93d7d
rustydb <doomslayer@hpe.com>
2021-09-29 08:39:59 -0500

    CASMREL-776

---
+++ casmrel-776_csm12-ncn-boot-order-backport/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+type curl 2>&1 >/dev/null || echo 2>& 'missing curl! script will fail'
+PIT_VER=1.2.2-1
+ILO_VER=3.2.3-1
+
+function getdeps {
+    curl -O https://packages.nmn/repository/casmrel-776/pit-init-${PIT_VER}.noarch.rpm
+    curl -O https://packages.nmn/repository/casmrel-776/ilorest-${ILO_VER}.x86_64.rpm
+}
