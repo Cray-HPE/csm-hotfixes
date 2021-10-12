5dc005c8fa07e1b145ac7812434c64911fa6553d
rustydb <doomslayer@hpe.com>
2021-10-12 15:08:19 -0500

    Remove directory that was removed already and brought in by mistake somehow

--- casmrel-776_csm12-ncn-boot-order-backport/lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# # This file was copied from GitHub.
-# permalink: https://github.com/Cray-HPE/node-image-build/blob/a260905eb451d2bbf1a098d2f1f5cbde08d89b4c/boxes/ncn-common/files/scripts/common/lib.sh
-
-function mprint {
-    printf '[% -25s] %s\n' "$0" "$1"
-}
