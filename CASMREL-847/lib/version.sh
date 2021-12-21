2a253478086770352048f887dc0349ec731e6992
Di Zou <ahoy@algol60.net>
2021-12-21 13:41:36 -0500

    final clean up

--- CASMREL-847/lib/version.sh
+++ CASMREL-847/lib/version.sh
@@ -2,7 +2,7 @@
 
 # Copyright 2020 Hewlett Packard Enterprise Development LP
 
-: "${RELEASE:="${RELEASE_NAME:="casmrel-847"}-${RELEASE_VERSION:="0.9.29999"}"}"
+: "${RELEASE:="${RELEASE_NAME:="casmrel-847"}-${RELEASE_VERSION:="1.0.0"}"}"
 
 # return if sourced
 return 0 2>/dev/null
